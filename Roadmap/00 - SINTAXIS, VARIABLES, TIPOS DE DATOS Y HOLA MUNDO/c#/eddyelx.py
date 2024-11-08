ed20b87c71773ec2160c00ff0baca33795453b64
eddyelx <eduelx84@gmail.com>
2024-11-08 02:46:08 +0100

    eddyelx.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/eddyelx.py
@@ -0,0 +1,28 @@
+# https://www.python.org
+
+# empiezo a programar
+
+"""
+esto es
+un comentario 
+sobre programación
+"""
+'''
+esto es
+un comentario 
+sobre programación
+'''
+
+my_variable = "myvariable"
+my_variable = "nuevo valor de mi varaiable"
+
+MY_CONSTANT = "mi constante" # por convención
+
+my_int = 1
+my_float= 1,5
+my_bool = true
+my_bool = false
+my_string = "mi cadena de texto"
+my_other_string = 'mi otra cadena de texto'
+
+print '¡hola,python!'
